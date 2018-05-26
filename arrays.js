f0ac7e2769ee091dd2e730742bac18744a1d09a5
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 01:43:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ];
 
 function addElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift(element);
+  return [element...,]
 }
 addElementToBeginningOfArray(chocolateBars, 'hi');
 
